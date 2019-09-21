51f1a7dd754065bc91b989c8d265e5e855ea6ba8
liuzheng644607 <793253892@qq.com>
2019-09-21 21:11:26 +0800

    feat: share

--- source/demo/remote/index.js
+++ source/demo/remote/index.js
@@ -1,99 +1,90 @@
-let desktopCapturer;
-if (isInElectron()) {
-  let electron = require("electron");
-  desktopCapturer = electron.desktopCapturer;
-}
+(function() {
+  const localPeerKey = "localPeerKey";
+  const peerIdEl = document.getElementById("js-peer-id");
+  const inputEl = document.getElementById("js-input-id");
+  const btnConnEl = document.getElementById("js-btn-connect");
+  const btnVideoConnEL = document.getElementById("js-btn-video");
+  const jsRemoteVideoEl = document.getElementById("js-remote-video");
+  const jsSelfVideoEl = document.getElementById("js-self-video");
+  const jsSharedScreenTips = document.getElementById('js-shared-screen-tips');
+  const jsViewSharedScreenBtn = document.getElementById('js-view-shared-screen');
 
-const localPeerKey = "localPeerKey";
-const peerIdEl = document.getElementById("js-peer-id");
-const inputEl = document.getElementById("js-input-id");
-const btnConnEl = document.getElementById("js-btn-connect");
-const btnVideoConnEL = document.getElementById("js-btn-video");
-const jsRemoteVideoEl = document.getElementById("js-remote-video");
-const jsSelfVideoEl = document.getElementById("js-self-video");
-const CALL_TYPE = {
-  video: "VIDEO",
-  shareScreen: "share-screen"
-};
+  const CALL_TYPE = {
+    video: "VIDEO",
+    shareScreen: "share-screen"
+  };
+  const sharedScreenMap = {};
+  let lastPeerId = localStorage.getItem(localPeerKey);
+  let newWindow = null;
 
-var getUserMedia = (function() {
-  var getUserMedia =
-    window.navigator.getUserMedia ||
-    window.navigator.webkitGetUserMedia ||
-    window.navigator.mozGetUserMedia ||
-    window.navigator.msGetUserMedia;
-  if (!getUserMedia) {
-    throw new Error("您的浏览器不支持getUserMedia");
+  function closeWindow() {
+    newWindow && newWindow.close();
+    newWindow = null;
   }
-  return getUserMedia.bind(navigator);
-})();
 
-let lastPeerId = localStorage.getItem(localPeerKey);
-let newWindow = null;
+  const peer = new Peer(lastPeerId, {
+    // host: "peer.lyan.me",
+    // secure: false,
+    // port: 9000,
+    debug: 3
+  });
 
-function closeWindow() {
-  newWindow && newWindow.close();
-  newWindow = null;
-}
+  peer.on("open", id => {
+    lastPeerId = id;
+    localStorage.setItem(localPeerKey, id);
+    peerIdEl.innerText = id;
+  });
 
-const peer = new Peer(lastPeerId, {
-  // host: "peer.lyan.me",
-  // secure: false,
-  // port: 9000,
-  debug: 3
-});
+  peer.on("call", mediaConnection => {
+    const type = mediaConnection.metadata.type;
+    switch (type) {
+      case CALL_TYPE.shareScreen:
+        receiveScreen(mediaConnection);
+        break;
+      case CALL_TYPE.video:
+        if (confirm(`${mediaConnection.peer}想与您视频通话`)) {
+          shareVideo(mediaConnection);
+        }
+        break;
+    }
+  });
 
-peer.on("open", id => {
-  lastPeerId = id;
-  localStorage.setItem(localPeerKey, id);
-  peerIdEl.innerText = id;
-});
+  peer.on("disconnected", e => {
+    console.log("已断开连接", e);
+  });
 
-peer.on("call", mediaConnection => {
-  const type = mediaConnection.metadata.type;
-  switch (type) {
-    case CALL_TYPE.shareScreen:
-      if (confirm(`${mediaConnection.peer}请求您分享屏幕`)) {
-        shareScreen(mediaConnection);
-      }
-      break;
-    case CALL_TYPE.video:
-      if (confirm(`${mediaConnection.peer}想与您视频通话`)) {
-        shareVideo(mediaConnection);
-      }
-      break;
-  }
-});
+  peer.on("error", e => {
+    const message = e.message;
+    if (/is taken/.test(message)) {
+      localStorage.setItem(localPeerKey, "");
+      window.location.reload();
+    } else {
+      alert(e);
+    }
+    closeWindow();
+  });
 
-peer.on("disconnected", e => {
-  console.log("已断开连接", e);
-});
+  btnConnEl.addEventListener("click", () => {
+    const remoteId = getRemoteId();
+    shareSelfScreen(remoteId);
+  });
 
-peer.on("error", e => {
-  const message = e.message;
-  if (/is taken/.test(message)) {
-    localStorage.setItem(localPeerKey, '');
-    window.location.reload();
-  } else {
-    alert(e);
-  }
-  closeWindow();
-});
+  btnVideoConnEL.addEventListener("click", () => {
+    const remoteId = getRemoteId();
+    videoConnect(remoteId);
+  });
 
-btnConnEl.addEventListener("click", () => {
-  const remoteId = getRemoteId();
-  callRemote(remoteId);
-});
+  jsViewSharedScreenBtn.addEventListener('click', function(e) {
+    const peerId = e.target.dataset.peerId;
+    createScreenWindow(peerId);
+  })
 
-btnVideoConnEL.addEventListener("click", () => {
-  const remoteId = getRemoteId();
-  videoConnect(remoteId);
-});
-
-function videoConnect(peerId) {
-  getUserMedia(
-    { video: true, audio: true },
-    stream => {
+  /**
+   * 视频通话请求
+   * @param {*} peerId 
+   */
+  function videoConnect(peerId) {
+    Device.getUserMedia({ video: true, audio: true }).then(stream => {
       var call = peer.call(peerId, stream, {
         metadata: {
           type: CALL_TYPE.video
@@ -103,140 +94,124 @@ function videoConnect(peerId) {
         renderRemoteVideo(createVideo(remoteStream));
       });
       renderSelfVideo(createVideo(stream));
-    },
-    err => {
-      console.log("Failed to get local stream", err);
-    }
-  );
-}
-
-function callRemote(peerId) {
-  const canvas = document.createElement("canvas");
-  const stream = canvas.captureStream(~~(Math.random() * 100));
-  const mediaConnection = peer.call(peerId, stream, {
-    metadata: {
-      type: CALL_TYPE.shareScreen
-    }
-  });
-  mediaConnection.on("stream", remoteStream => {
-    newWindow = window.open(
-      peerId,
-      "",
-      "toobar=no,menubar=no,scrollbars=no,location=no,status=no"
-    );
-    setTimeout(() => {
-      if (newWindow) {
-        newWindow.document.body.innerHTML = "";
-        newWindow.document.body.appendChild(createVideo(remoteStream));
-      }
-    }, 1500);
-
-    newWindow.onclose = () => {
-      mediaConnection.close();
-    };
-  });
-
-  mediaConnection.on("close", e => {
-    closeWindow();
-  });
-
-  mediaConnection.on("error", e => {
-    closeWindow();
-    alert(e);
-  });
-}
-
-function getRemoteId() {
-  const remoteId = inputEl.value;
-  if (!remoteId || lastPeerId === remoteId) {
-    alert("请输入识别码");
-    throw new Error("请输入识别码");
+    });
   }
 
-  return remoteId;
-}
-
-function createVideo(stream) {
-  const videoEl = document.createElement("video");
-  videoEl.srcObject = stream;
-  videoEl.autoplay = true;
-  return videoEl;
-}
-
-function shareScreen(mediaConnection) {
-  if (isInElectron()) {
-    shareElectronScreen(mediaConnection);
-  } else {
-    shareBrowserScreen(mediaConnection);
-  }
-}
-
-function shareBrowserScreen(mediaConnection) {
-  navigator.mediaDevices.getDisplayMedia().then(stream => {
-    mediaConnection.answer(stream);
-  });
-}
-
-function shareElectronScreen(mediaConnection) {
-  desktopCapturer.getSources(
-    { types: ["window", "screen"] },
-    (err, sources) => {
-      const selectedSource = sources[0];
-      constraints = {
-        audio: false,
-        video: {
-          mandatory: {
-            chromeMediaSource: "desktop",
-            chromeMediaSourceId: selectedSource.id,
-            maxWidth: screen.availWidth,
-            maxHeight: screen.availHeight,
-            maxFrameRate: 100,
-            minFrameRate: 60
-          }
+  /**
+   * 分享自己的屏幕
+   */
+  function shareSelfScreen(peerId) {
+    Device.getScreenStream().then((stream) => {
+      const mediaConnection = peer.call(peerId, stream, {
+        metadata: {
+          type: CALL_TYPE.shareScreen
         }
-      };
-      navigator.getUserMedia(
-        constraints,
-        steam => {
-          mediaConnection.answer(steam);
-        },
-        handleErr
-      );
-    }
-  );
-}
+      });
+      //  取消分享，断开连接
+      stream.oninactive = () => {
+        mediaConnection.close();
+      }
+    });
+  }
 
-function shareVideo(mediaConnection) {
-  getUserMedia(
-    { video: true, audio: true },
-    function(stream) {
+  function receiveScreen(mediaConnection) {
+    const peerId = mediaConnection.peer;
+    if (sharedScreenMap[peerId]) {
+      createScreenWindow(peerId);
+      return;
+    }
+    const canvas = document.createElement("canvas");
+    const stream = canvas.captureStream(~~(Math.random() * 100));
+    mediaConnection.answer(stream);
+    mediaConnection.on("stream", remoteStream => {
+      sharedScreenMap[peerId] = remoteStream;
+      showScreenTips(peerId);
+    });
+
+    mediaConnection.on("close", e => {
+      sharedScreenMap[peerId] = null;
+      hideScreenTips();
+      closeWindow();
+    });
+
+    mediaConnection.on("error", e => {
+      sharedScreenMap[peerId] = null;
+      hideScreenTips();
+      closeWindow();
+    });
+  }
+
+  function getRemoteId() {
+    const remoteId = inputEl.value;
+    if (!remoteId || lastPeerId === remoteId) {
+      alert("请输入识别码");
+      throw new Error("请输入识别码");
+    }
+
+    return remoteId;
+  }
+
+  function createVideo(stream) {
+    const videoEl = document.createElement("video");
+    videoEl.srcObject = stream;
+    videoEl.autoplay = true;
+    return videoEl;
+  }
+
+  function shareVideo(mediaConnection) {
+    Device.getUserMedia({ video: true, audio: true }).then(stream => {
       mediaConnection.answer(stream);
       mediaConnection.on("stream", function(remoteStream) {
         renderRemoteVideo(createVideo(remoteStream));
       });
       renderSelfVideo(createVideo(stream));
-    },
-    function(err) {
-      console.log("Failed to get local stream", err);
+    });
+  }
+
+  function renderSelfVideo(video) {
+    jsSelfVideoEl.innerHTML = "";
+    jsSelfVideoEl.appendChild(video);
+  }
+
+  function renderRemoteVideo(video) {
+    jsRemoteVideoEl.innerHTML = "";
+    jsRemoteVideoEl.appendChild(video);
+  }
+
+  function showScreenTips(peerId) {
+    jsSharedScreenTips.style.display = '';
+    jsViewSharedScreenBtn.dataset.peerId = peerId;
+  }
+
+  function hideScreenTips() {
+    jsSharedScreenTips.style.display = 'none';
+  }
+
+  function createScreenWindow(peerId) {
+    const remoteStream = sharedScreenMap[peerId];
+    if (!remoteStream) {
+      alert("没有人分享屏幕");
+      return;
     }
-  );
-}
-
-function renderSelfVideo(video) {
-  jsSelfVideoEl.innerHTML = "";
-  jsSelfVideoEl.appendChild(video);
-}
-
-function renderRemoteVideo(video) {
-  jsRemoteVideoEl.innerHTML = "";
-  jsRemoteVideoEl.appendChild(video);
-}
-
-function isInElectron() {
-  return window.process && window.process.type === "renderer";
-}
-
-function handleErr(err) {
-  console.log(err);
-}
-
+    if (newWindow) {
+      newWindow.close();
+    }
+    newWindow = window.open(
+      'screen.html',
+      "",
+      "toobar=no,menubar=no,scrollbars=no,location=no,status=no"
+    );
+    if (newWindow) {
+      newWindow.onclose = () => {
+        newWindow = null;
+        mediaConnection.close();
+      };
+      newWindow.onload = () => {
+        if (newWindow) {
+          newWindow.document.body.innerHTML = "";
+          newWindow.document.body.appendChild(createVideo(remoteStream));
+        }
+      };
+    }
+  }
+})();
